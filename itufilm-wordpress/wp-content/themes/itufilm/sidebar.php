0678275d7df9ef12017f484048face508660fa65
Mathias Brandt <maini123@gmail.com>
2015-04-13 18:57:15 +0200

    implemented the sidebar

--- itufilm-wordpress/wp-content/themes/itufilm/sidebar.php
+++ itufilm-wordpress/wp-content/themes/itufilm/sidebar.php
@@ -1,9 +1,43 @@
-<aside id="sidebar" role="complementary">
-<?php if ( is_active_sidebar( 'primary-widget-area' ) ) : ?>
-<div id="primary" class="widget-area">
-<ul class="xoxo">
-<?php dynamic_sidebar( 'primary-widget-area' ); ?>
-</ul>
+<div class="sidebar col-md-4 col-xs-12">
+    <!-- next event -->
+    <div class="item">
+        <div class="item-heading">
+            Next Event
+        </div>
+
+        <div class="item-image">
+            <img src="<?php echo get_template_directory_uri(); ?>/img/wallflower.jpg" />
+        </div>
+
+        <div class="item-content next-event-content">
+            The Perks of Being a Wallflower
+            <br />
+            16:00
+            <br />
+            February 13th, 2015
+            <br />
+            Auditorium 1
+        </div>
+    </div>
+    <!-- /next event -->
+
+    <!-- where to find us -->
+    <div class="item">
+        <div class="item-heading">
+            Where to Find Us
+        </div>
+
+        <div class="item-image">
+            <img src="<?php echo get_template_directory_uri(); ?>/img/map.png" />
+        </div>
+
+        <div class="item-content">
+            Café Analog, IT University of Copenhagen
+            <br />
+            Rued Langgaards Vej 7
+            <br />
+            2300 Copenhagen S
+        </div>
+    </div>
+    <!-- /where to find us -->
 </div>
-<?php endif; ?>
-</aside>
